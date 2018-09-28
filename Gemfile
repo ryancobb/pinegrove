8196af9a3895e9cbe9f5804a63cff16d9768d67c
Ryan Cobb <ryan.cobb@mx.com>
2018-09-28 09:38:21 -0600

    any day now...

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+gem "fuzzy-string-match", :require => "fuzzystringmatch"
+gem "httparty"
+gem "pry"
+gem "ruby-progressbar"
---
+++ pinegrove.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+
+Bundler.require(:default)
+
+string_distance_threshold = 0.80
+
+url = "https://pinegrove.bandcamp.com/"
+html_page = ::HTTParty.get(url)
+jarrow = ::FuzzyStringMatch::JaroWinkler.create(:native)
+
+loop do
+  new_html_page = ::HTTParty.get(url)
+  string_distance = jarrow.getDistance(html_page, new_html_page)
+  puts string_distance
+  if string_distance < string_distance_threshold
+    puts %{
+                                              +++++++++++    + +    ++  +   + +                      
+                                              +                               +                      
+                                              +                               /                      
+                                              +                               +                      
+                                              +                               ++                     
+                          +  + ++++++++ ++++++/+++++++++++                    ++                     
+                        ++                    +          +                    ++                     
+                        +                     +          /                    ++                     
+                        +                     +          /                    ++                     
+                        +                     +          /                    ++                     
+                        +                     +          +                     +                     
+                        +                     +          +                    ++                     
+                        +                     +          +                    ++                     
+                        +                     +          +                    ++                     
+                        +                     +          +                    ++                     
+                        +                     +          /                    ++                     
+                        +                     ++  +++    /++++ +++++++++++ ++++                      
+                        +                                +                                           
+                        +                                /                                           
+                        +                                /                                           
+                        +                                +                                           
+                        +                                /                                           
+                        +  +   +    + ++  +++++++ +++++++                                              
+    }
+  break                                                                                                    
+  else
+    progressbar = ::ProgressBar.create(:title => "Sleeping", :remainder_mark => "-")
+    100.times { progressbar.increment; sleep 0.1 }
+  end
+end
